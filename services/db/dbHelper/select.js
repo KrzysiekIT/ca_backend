a37a4606f85ab1ecee47514a245f8153f29bc507
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-27 23:41:35 +0100

    Refactor change arguments into object

--- services/db/dbHelper/select.js
+++ services/db/dbHelper/select.js
@@ -1,7 +1,7 @@
 const db = require("@/db/connection.js");
 const getDbResult = require("./getDbResult.js")
 
-const select = (cb, table, columns) => {
+const select = ({cb, table, columns}) => {
   columns = columns.join(", ");
   db.query(`SELECT ${columns} FROM ${table};`, getDbResult(cb));
 };
