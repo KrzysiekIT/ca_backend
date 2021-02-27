24a4fd9fec251704d3e02510698cdb05c9476779
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-28 00:21:27 +0100

    Refactor change get users to new function

--- services/api/users/index.js
+++ services/api/users/index.js
@@ -1,15 +1,20 @@
 const { Router } = require("express");
 const router = Router();
-const db = require("@/db/connection.js");
+const db = require("@/db/dbHelper");
 const permit = require("@/auth/permit");
 
 router.get("/", permit(3), (req, res) => {
-  db.query("select * from users", (error, results) => {
-    if (error) {
-      return res.status(500).json({ type: "error", error });
-    }
-    res.json({ type: "success", message: "Users selected", results });
-  });
+  const selectOptions = {
+    cb: (err, response) => {
+      if (err) {
+        res.status(500).json({ message: "Database error" });
+      }
+      res.json(response);
+    },
+    table: "users",
+    columns: ["id", "email", "name", "surname", "phone"],
+  };
+  db.select(selectOptions);
 });
 
 module.exports = router;
