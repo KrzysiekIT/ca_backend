3cb86c013d9be63a68dbdd276dfb903362bb249d
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-28 02:26:07 +0100

    Feat add update db functions

--- services/db/dbHelper/makeDbQuery.js
+++ services/db/dbHelper/makeDbQuery.js
@@ -1,7 +1,28 @@
 const makeDbQuery = (type) => {
+  const prepare = {
+    updateSet: (newValues) => {
+      const keysValues = [];
+      Object.keys(newValues).forEach((columnName) => {
+        keysValues.push(`${columnName} = ${newValues[columnName]}`);
+      });
+      return keysValues.join(", ");
+    },
+    updateConditions: (conditions) => {
+      return conditions
+        .map(({ field, condition, value }) => {
+          return `${field} ${condition} ${value}`;
+        })
+        .join(" AND ");
+    },
+  };
   const types = {
     select: (options) => {
       return `SELECT ${options.columns.join(", ")} FROM ${options.table};`;
+    },
+    update: (options) => {
+      return `UPDATE ${options.table} SET ${prepare.updateSet(
+        options.newValues
+      )} WHERE ${prepare.updateConditions(options.conditions)}`;
     },
   };
   return (options) => {
