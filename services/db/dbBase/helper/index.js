5927f7be1afb0fa61eb7179091e0def74fbdeebb
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-28 02:59:39 +0100

    Refactor remove unused import

--- services/db/dbBase/helper/index.js
+++ services/db/dbBase/helper/index.js
@@ -1,6 +1,5 @@
 const { join } = require("path");
 const { readdirSync, statSync } = require("fs");
-const { dir } = require("console");
 const getDirs = (p) => readdirSync(p).filter((f) => statSync(join(p, f)));
 
 const dirs = getDirs(__dirname).filter((dir) => dir !== "index.js");
