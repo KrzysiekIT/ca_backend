49417a285b05147285237c898ccad7969930a6a4
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-02-28 03:00:32 +0100

    Refactor removed unused db helper import

--- services/api/users/index.js
+++ services/api/users/index.js
@@ -3,10 +3,8 @@ const router = Router();
 const db = require("@/db/dbBase");
 const cb = require("@/api/helper");
 const permit = require("@/auth/permit");
-const helper = require("@/db/dbBase/helper");
 
 router.get("/", permit(3), (req, res) => {
-  console.log(helper)
   const selectOptions = {
     cb: cb(res),
     table: "users",
