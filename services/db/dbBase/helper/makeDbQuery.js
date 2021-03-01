32f03b1b3cb4fb40de06a4bfdd97c0943c701aae
Krzysiek <krzysztof.karczewski@protonmail.com>
2021-03-01 19:18:24 +0100

    Feat add create in db function

--- services/db/dbBase/helper/makeDbQuery.js
+++ services/db/dbBase/helper/makeDbQuery.js
@@ -20,10 +20,30 @@ const makeDbQuery = () => {
         })
         .join(" AND ");
     },
+    create: (values) => {
+      const keys = [];
+      const keysValues = [];
+      Object.keys(values).forEach((columnName) => {
+        if (typeof values[columnName] === "string") {
+          values[columnName] = `'${values[columnName]}'`;
+        }
+        keys.push(columnName);
+        keysValues.push(values[columnName]);
+      });
+      return `(${keys.join(", ")}) VALUES (${keysValues.join(", ")})`;
+    },
   };
   const types = {
     select: ({ columns, table }) => {
       return `SELECT ${columns.join(", ")} FROM ${table};`;
+    },
+    selectWhere: ({ columns, table, conditions }) => {
+      return `SELECT ${columns.join(
+        ", "
+      )} FROM ${table} WHERE ${prepare.updateConditions(conditions)};`;
+    },
+    create: ({ table, values }) => {
+      return `INSERT INTO ${table} ${prepare.create(values)};`;
     },
     update: ({ table, newValues, conditions }) => {
       return `UPDATE ${table} SET ${prepare.updateSet(
