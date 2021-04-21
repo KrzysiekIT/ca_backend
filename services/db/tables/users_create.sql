9335f902728f1f90996bd03b4dff24c485f51187
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-04-22 00:47:32 +0200

    remove trainer_id field from users table

--- services/db/tables/users_create.sql
+++ services/db/tables/users_create.sql
@@ -9,7 +9,6 @@ CREATE TABLE IF NOT EXISTS `users` (
     `parent_full_name` varchar(255),
     `parent_email` varchar(255),
     `parent_phone_number` varchar(255),
-    `trainer_id` int(11) DEFAULT NULL,
     `lesson_day` tinyint(3),
     `status` tinyint(3),
     `start_at` datetime DEFAULT NULL,
@@ -19,7 +18,6 @@ CREATE TABLE IF NOT EXISTS `users` (
     `group_id` int(11) DEFAULT NULL,
     `terms_accepted` tinyint(1) DEFAULT 0,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`trainer_id`) REFERENCES `users`(`id`),
     FOREIGN KEY (`role_id`) REFERENCES `roles`(`id`),
     FOREIGN KEY (`group_id`) REFERENCES `groups`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
