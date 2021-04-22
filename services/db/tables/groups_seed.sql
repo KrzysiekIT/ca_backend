6f57c9b900b95c5811bd9b4d28ab20a31ffb18fa
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-04-23 00:11:22 +0200

    add name column to groups

--- services/db/tables/groups_seed.sql
+++ services/db/tables/groups_seed.sql
@@ -1,5 +1,5 @@
 START TRANSACTION;
-INSERT INTO `groups` (`id`, `trainer_id`, `lesson_day`, `lesson_hour`, `lesson_link`)
-VALUES (1, 3, 4, '18:00', 'https://google.com'),
-    (2, 3, 5, '11:15', 'https://google.pl');
+INSERT INTO `groups` (`id`, `name`, `trainer_id`, `lesson_day`, `lesson_hour`, `lesson_link`)
+VALUES (1, 'cz. 18:00' , 3, 4, '18:00', 'https://google.com'),
+    (2, 'pt. 11:15', 3, 5, '11:15', 'https://google.pl');
 COMMIT;
