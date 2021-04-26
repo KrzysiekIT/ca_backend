83bcd8da69c81453d8cff148b9c851807bfc87b7
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-04-26 15:06:22 +0200

    add example to group

--- services/db/tables/groups_seed.sql
+++ services/db/tables/groups_seed.sql
@@ -1,5 +1,6 @@
 START TRANSACTION;
 INSERT INTO `groups` (`id`, `label`, `trainer_id`, `lesson_day`, `lesson_hour`, `lesson_link`)
 VALUES (1, 'cz. 18:00' , 3, 4, '18:00', 'https://google.com'),
-    (2, 'pt. 11:15', 3, 5, '11:15', 'https://google.pl');
+    (2, 'pt. 11:15', 3, 5, '11:15', 'https://google.pl'),
+    (3, 'wt. 15:15', 3, 7, '19:15', 'https://meet.pl');
 COMMIT;
