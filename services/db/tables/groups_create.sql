ab7529bc4b7c1168747bbf3f6bb2fac73cfbe3d4
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-05-02 16:07:01 +0200

    add default values to group

--- services/db/tables/groups_create.sql
+++ services/db/tables/groups_create.sql
@@ -1,12 +1,12 @@
 START TRANSACTION;
 CREATE TABLE IF NOT EXISTS `groups` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `level` tinyint(3),
-    `label` varchar(255) NOT NULL,
-    `trainer_id` int(11) NOT NULL,
-    `lesson_day` tinyint(3),
-    `lesson_hour` varchar(255),
-    `lesson_link` varchar(255),
+    `level` tinyint(3) DEFAULT 1,
+    `label` varchar(255)  DEFAULT "",
+    `trainer_id` int(11)  DEFAULT 3,
+    `lesson_day` tinyint(3) DEFAULT 1,
+    `lesson_hour` varchar(255) DEFAULT "",
+    `lesson_link` varchar(255) DEFAULT "",
     PRIMARY KEY (`id`),
     FOREIGN KEY (`trainer_id`) REFERENCES `users`(`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
