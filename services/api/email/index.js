9f039efbac0b8b9b63264de34364f529d4be2f5e
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-05-20 19:25:23 +0200

    change email data:

--- services/api/email/index.js
+++ services/api/email/index.js
@@ -2,12 +2,12 @@ const { Router, request } = require("express");
 const router = Router();
 const sendEmail = require("@/email");
 
-router.post("/", (req, res) => {
+router.post("/reset", (req, res) => {
   const mailOptions = {
     from: "ciekawenewslettery@gmail.com",
-    to: "krzykarc@gmail.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!",
+    to: `${req.body.email}`,
+    subject: "Champions academy - resetowanie hasła",
+    html: `<a href="www.google.com" target="_blank">Kliknij, aby zresetować hasło.</a>`,
   };
   sendEmail(mailOptions);
   res.json({ message: "Email has been sent" });
