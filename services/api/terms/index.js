aa4e4ff953eacc71f22daedba9fc6fcd47d4471b
KrzysiekIT <krzysztof.karczewski@pm.me>
2021-05-20 22:47:03 +0200

    add terms of use endpoint

---
+++ services/api/terms/index.js
@@ -0,0 +1,28 @@
+const { Router, request } = require("express");
+const router = Router();
+const db = require("@/db/dbBase");
+const cb = require("@/api/helper");
+const permit = require("@/auth/permit");
+
+router.get("/", permit(15), (req, res) => {
+  const options = {
+    cb: cb(res),
+    table: "terms",
+    type: "select",
+    columns: ["body"],
+  };
+  db(options);
+});
+
+router.put("/", permit(3), (req, res) => {
+  const options = {
+    cb: cb(res),
+    table: "terms",
+    type: "update",
+    newValues: req.body.newValues,
+    conditions: [{ field: "id", condition: "=", value: 1 }],
+  };
+  db(options);
+});
+
+module.exports = router;
